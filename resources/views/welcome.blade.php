fb6cc2610ed6397214a7a7a092f9dc13cc081b6f
yotadaa <mukhtadanasution@gmail.com>
2024-12-30 13:04:31 +0700

    project added

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Rumah Pustaka</title>
+</head>
+
+<body>
+    <div class="flex justify-center items-center h-dvh">
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    </div>
+</body>
+
+</html>
